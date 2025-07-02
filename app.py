622e20e7ef5a5dc2e8ba50757f5165edfec49c14
Grigory <greg.shvechkov@gmail.com>
2025-07-02 16:29:20 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/roblox-data', methods=['POST'])
+def roblox_data():
+    data = request.get_json()
+    print("✅ Received from Roblox:", data)
+    return jsonify({"status": "success"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)  # Render uses port 10000
